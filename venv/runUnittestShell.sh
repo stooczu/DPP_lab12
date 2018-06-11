0f668c2159487ff5f1833c0dc4a2df47886c2d90
fkaszak <226091@studnet.pwr.edu.pl>
2018-06-11 12:21:23 +0200

    DPP_lab12_2

--- venv/runUnittestShell.sh
+++ venv/runUnittestShell.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-
+cd /venv
 
 find . -name "*run_test_module.py" -print | while read f; do
         echo "$f"
@@ -10,5 +10,5 @@ find . -name "*run_test_module.py" -print | while read f; do
         ###
 done
 
-cp -r ./coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
+cp -r coverage.xml /var/lib/jenkins/workspace/DPP_lab12/coverage.xml
 cp -r ./python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
