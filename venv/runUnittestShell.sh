48561d016797876f8be284a3cc7724249688ad88
fkaszak <226091@studnet.pwr.edu.pl>
2018-06-11 12:33:30 +0200

    DPP_lab12_2

--- venv/runUnittestShell.sh
+++ venv/runUnittestShell.sh
@@ -2,5 +2,5 @@
 
 cd /venv
 python run_test_module.py -o coverage.xml
-cp -r coverage.xml /var/lib/jenkins/workspace/DPP_lab12/coverage.xml
+##cp -r coverage.xml /var/lib/jenkins/workspace/DPP_lab12/coverage.xml
 ##cp -r ./python_unittests_xml /var/lib/jenkins/workspace/DPP_lab12/python_unittests_xml
