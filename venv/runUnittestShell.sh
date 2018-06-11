ff87f9fa755af1197b957e5143661639057192a9
fkaszak <226091@studnet.pwr.edu.pl>
2018-06-11 14:54:21 +0200

    DPP_lab12_final

--- venv/runUnittestShell.sh
+++ venv/runUnittestShell.sh
@@ -3,8 +3,9 @@ set -e
 
 virtualenv venv --distribute -p python
 
-pip install -r venv/requirements.txt
-PYTHONPATH=. venv/bin/coverage run run_test_module.py
-PYTHONPATH=. venv/bin/coverage report --omit=venv/*
-PYTHONPATH=. venv/bin/coverage html --omit=venv/*
-PYTHONPATH=. venv/bin/coverage xml --omit=venv/*
+pip install -r xmlrunner==1.7.7
+pip install -r coverage==5.0a1
+PYTHONPATH=. venv run_test_module.py
+PYTHONPATH=. venv report --omit=venv/*
+PYTHONPATH=. venv html --omit=venv/*
+PYTHONPATH=. venv xml --omit=venv/*
